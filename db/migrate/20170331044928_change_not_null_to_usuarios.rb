fcd0d8afa44bceee7ebae815461cd6121571b862
luiz.quelves@gmail.com <lq872913>
2017-03-31 02:21:54 -0300

    refactory migrations

--- db/migrate/20170331044928_change_not_null_to_usuarios.rb
+++ db/migrate/20170331044928_change_not_null_to_usuarios.rb
@@ -1,6 +1,4 @@
 class ChangeNotNullToUsuarios < ActiveRecord::Migration[5.0]
   def change
-    change_column_null :usuarios, :nombre, :null => false
-    change_column_null :usuarios, :apelido, :null => false
   end
 end
