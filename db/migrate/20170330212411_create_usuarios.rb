bf1dc18773a9ada861f56728773ff2f808d54e7f
luiz.quelves@gmail.com <lq872913>
2017-03-31 01:40:13 -0300

    add not null for nombre and apelido

--- db/migrate/20170330212411_create_usuarios.rb
+++ db/migrate/20170330212411_create_usuarios.rb
@@ -1,11 +1,10 @@
 class CreateUsuarios < ActiveRecord::Migration[5.0]
   def change
     create_table :usuarios do |t|
-      t.string :nombre
-      t.string :apellido
+      t.string :nombre, null => false
+      t.string :apellido, null => false
       t.string :usuario
       t.string :twitter
-
       t.timestamps
     end
   end
---
+++ db/migrate/20170331034721_change_nombre_n_ull_to_usuarios.rb
@@ -0,0 +1,4 @@
+class ChangeNombreNUllToUsuarios < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
